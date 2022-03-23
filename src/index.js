3baf06182029cdf8f616d782e83afd8e5d9a2d27
Carlos G <cgonz490@stu.southtexascollege.edu>
2022-03-23 16:41:04 -0500

    added router

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import cors from 'cors'
 import config from './utils/config'
 import logger from './utils/logger'
 import errors from './utils/errors'
+// import res from 'express/lib/response'
 
-//import router from './routes'
+import router from './routes'
 
 const app = express()
 
